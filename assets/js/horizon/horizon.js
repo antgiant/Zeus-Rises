6ed7920c90803be9b517305c33e91b32a3284795
antgiant <antgiant@gmail.com>
2025-11-21 09:42:51 -0500

    Move CSS to JS as the JS was overwriting the CSS

--- assets/js/horizon/horizon.js
+++ assets/js/horizon/horizon.js
@@ -15,4 +15,7 @@ export function refreshSky() {
   const [gradient, topVec, bottomVec] = renderGradient(sunPos.altitude);
 
   document.body.style.background = `${gradient}, url('../img/stars.png'), rgb(${topVec[0]}, ${topVec[1]}, ${topVec[2]})`;
+  document.body.style.backgroundRepeat = `no-repeat, repeat, no-repeat`;
+  document.body.style.backgroundSize = `cover, auto, auto`;
+  document.body.style.backgroundAttachment = `fixed, scroll, scroll`;
 }
