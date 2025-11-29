e4e0af326aa494e41b54f6b19bfd4324f83f97c4
antgiant <antgiant@gmail.com>
2025-11-29 16:06:32 -0500

    Another attempt to fix dark mode

--- assets/js/global.js
+++ assets/js/global.js
@@ -3,4 +3,6 @@ import { refreshSky } from './horizon/horizon.js'
 refreshSky();
 
 // Listen for changes to dark mode preference and update the sky color
-mediaQuery.addEventListener("change", refreshSky);
+const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+mediaQuery.addEventListener('change', refreshSky);
